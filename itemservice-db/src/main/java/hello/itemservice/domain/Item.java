cf95d03a97d83c35944f72f23345898d81efb53a
RadiantK <pps8853@gmail.com>
2022-10-05 19:42:08 +0900

    jpa 를 사용한 crud 구현 및 테스트

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,12 +2,20 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity // jpa 객체로 지정
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 컬럼명과 필드값 매핑
+    @Column(name = "item_name", length = 20)
     private String itemName;
+    // 필드값과 컬럼명이 같으면 생략 가능
     private Integer price;
     private Integer quantity;
 
